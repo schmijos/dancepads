17f87f444d1a23a0018103aa40ffef329a6d6a99
schmijos <schmijos@gmx.ch>
2014-04-21 20:59:58 +0200

    types should be packed now but arent :-(

--- simulation/proteus/PIC18F24K22/dp_spitypes.h
+++ simulation/proteus/PIC18F24K22/dp_spitypes.h
@@ -1,24 +1,24 @@
 // pack content for SPI transfer
-union dp_std_command {
-        uint8_t is_rgb : 1;
-        struct {
-                uint8_t : 1;
-                uint8_t r : 2;
-                uint8_t g : 3;
-                uint8_t b : 2;
+#define __pack // should not be necessary for 8bit PICs
+
+typedef union {
+        unsigned is_rgb : 1;
+        __pack struct {
+                unsigned : 1;
+                unsigned r : 2;
+                unsigned g : 3;
+                unsigned b : 2;
         } rgb;
-        uint8_t byte;
+        unsigned byte;
         //unsigned int is_noop : 8 ^ 0xFF;
-};
-typedef union dp_std_command dp_std_command_t;
+} dp_std_command_t;
 
 // pack content for SPI transfer
-union dp_std_status {
-        uint8_t is_pressed : 1;
-        struct {
-                uint8_t : 1;
-                uint8_t value : 7;
+typedef union {
+        unsigned is_pressed : 1;
+        __pack struct {
+                unsigned : 1;
+                unsigned value : 7;
         } data;
-        uint8_t byte;
-};
-typedef union dp_std_status dp_std_status_t;
+        unsigned byte;
+} dp_std_status_t;
--- simulation/proteus/PIC18F24K22/main.c
+++ simulation/proteus/PIC18F24K22/main.c
@@ -53,22 +53,28 @@ void main(void)
     ConvertADC();
     cmd_from_master.byte = spi_tranceive(0);
     
-    // MAIN LOOP
+    /* MAIN LOOP:
+     * At first we read the ADC value of the piezo-weight sensors if already present. 
+     * Then we fill this data into a status package which is to be sent to our SPI master.
+     * After that we await a command from our SPI master and send our status to it (tranceive).
+     * If we got a color, we light our LEDs per PWM. If we got a command, we analyze it and take action.
+     */
     while (1) {
         if (!BusyADC()) {
             adc = ReadADC() >> 2; // we just need 8bits from the returned 10bit sample
-            ConvertADC();
+            ConvertADC(); // already start next conversion. Meanwhile we're gonna do stuff with SPI
         }
         
         current_status.data.value = adc;
-        current_status.is_pressed = 0; // 0 or 1
-        cmd_from_master.byte = spi_tranceive(current_status.byte); // TODO error handling needed: what if spi is not present?
+        current_status.is_pressed = 0; // TODO: Analyze the adc value and set this one to 0 or 1
+        cmd_from_master.byte = spi_tranceive(current_status.byte); // TODO: error handling needed: what if spi is not present? Will we operate on our own?
         if (cmd_from_master.is_rgb) {
             // we got a color
-            PORTCbits.RC2 = 1; // Debug LED: we got a color
+            PORTCbits.RC2 = 0; // Debug LED
+            led_set_rgb(cmd_from_master.rgb.r*10, cmd_from_master.rgb.g*10, cmd_from_master.rgb.b*10); // TODO: introduce multiplicator to reach 100% PWM
         } else {
             // we got a command
-            PORTCbits.RC2 = 0;
+            PORTCbits.RC2 = 1; // Debug LED
         }
     }
  }
